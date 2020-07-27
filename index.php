2a9e4c71b97568f0b16609f0354bb7917b3b4e16
Anaïs Rouvière <a.rouviere@hotmail.com>
2020-07-27 11:10:45 +0200

    Création du template de base

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php
+<?php ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Dynamic Jet</title>
+</head>
+<body>
+
+<script src="js/app.js"></script>
+</body>
+</html>
