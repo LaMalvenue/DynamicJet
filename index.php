feb238ae8a3fcd157690e1f19d99926b36f42f44
Anaïs Rouvière <a.rouviere@hotmail.com>
2020-07-27 12:26:33 +0200

    Mise en place du JS Bootstrap

--- index.php
+++ index.php
@@ -78,7 +78,10 @@
     </p>
 </div>
 
-
+<script src="https://bootswatch.com/_vendor/jquery/dist/jquery.min.js"></script>
+<script src="https://bootswatch.com/_vendor/popper.js/dist/umd/popper.min.js"></script>
+<script src="https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="https://bootswatch.com/_assets/js/custom.js"></script>
 <script src="js/app.js"></script>
 </body>
 </html>
